433f144301dfa1b7cd44ff8ed91c8b03e70037bf
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-17 16:22:21 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,12 +4,12 @@ def reformat_languages(languages)
   new_hash = {}
    languages.each do |style, description|
     description.each do |lang, type|
-      binding.pry
-      if new_hash.has_key?(lang)
+        if new_hash.has_key?(lang)
         new_hash[lang][:style] << style
       else
         new_hash[lang] = type
         new_hash[lang][:style] = [style]
+        binding.pry
       end
     end
   end
